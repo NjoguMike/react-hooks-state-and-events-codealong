17127c0c8f761927015a56e337a77dd6e4105359
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-08-25 17:45:53 +0300

    Complete React State Lab

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+const [click, setClick] = useState(false)
+
+function handleCLick(){
+  setClick(currentValue => !currentValue)
+ 
+}
+
+const color = click ? "red" : "white"
+
+  return <button onClick={handleCLick} style={{background: color}} >{click ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
